21bc48d065e44c5ebc89085652a73197da7dea83
Mateusinhodev <mateusdev23@gmail.com>
2025-05-19 12:26:21 -0300

    Criando página de login

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,7 +1,45 @@
+import './signin.css'
+
+import { useState } from 'react';
+
+import logo from '../../assets/react.svg'
+
+import { Link } from 'react-router-dom';
+
 function SignIn() {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+
     return(
-        <div>
-            <h1>Página Login</h1>
+        <div className='container-center'>
+            <div className='login'>
+                <div className='login-area'>
+                    <img src={logo} alt="Logo do Sistema de Chamados" />
+                </div>
+
+                <form>
+                    <h1>Entrar</h1>
+                    <input 
+                        type="text" 
+                        placeholder='email@email.com'
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}   
+                    />
+
+                    <input 
+                        type="password" 
+                        placeholder='********'
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}   
+                    />
+
+                    <button type='submit'>Acessar</button>
+                </form>
+
+                <Link to="/register">Criar uma conta</Link>
+            </div>
         </div>
     )
 }

9323586828f3bfbfdf23d5173322df08d9b61fc8
Mateusinhodev <mateusdev23@gmail.com>
2025-05-19 12:55:41 -0300

    Criando página de cadastro

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,7 +1,49 @@
+import { useState } from 'react';
+import logo from '../../assets/react.svg'
+import { Link } from 'react-router-dom';
+
 function SignUp() {
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+
     return(
-        <div>
-            <h1>Página Cadastro</h1>
+        <div className='container-center'>
+            <div className='login'>
+                <div className='login-area'>
+                    <img src={logo} alt="Logo do Sistema de Chamados" />
+                </div>
+
+                <form>
+                    <h1>Nova Conta</h1>
+                    <input 
+                        type="text" 
+                        placeholder='Seu nome'
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}   
+                    />
+
+                    <input 
+                        type="text" 
+                        placeholder='email@email.com'
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}   
+                    />
+
+                    <input 
+                        type="password" 
+                        placeholder='********'
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}   
+                    />
+
+                    <button type='submit'>Cadastrar</button>
+                </form>
+
+                <Link to="/register">Já possui uma conta? Faça login</Link>
+            </div>
         </div>
     )
 }
